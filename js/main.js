ae5f442f88adbd5a9192200351fdbcb4583a894c
ak29 <ak229@buffalo.edu>
2015-12-13 20:13:19 -0500

    Added ng-model tags and fixed a little UI

--- js/main.js
+++ js/main.js
@@ -60,6 +60,25 @@ app.controller('HomeCtrl', function ($scope, $rootScope, $location, $http) {
   //   return tags;
   // }
 
+  
+  $scope.placeRequest = function(){
+    var daad = document.getElementById('fromDate').value;
+   //var now = new Date();
+   var daa = new Date(daad);
+   dateFormat(daa,"longTime");
+   //var ddd = daa.format("isoDateTime");
+   var aaa = daa+'Z';
+  console.log("time is"+aaa);
+  var placeRequestURL = "http://192.168.0.6:8080/SolrSearch/solr/search";
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", placeRequestURL);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  var send_data_pre = JSON.stringify('{"query_sent":"syria refugee crysis","advanced":true,"advanced_attributes":{"tags":["syria","war","leader"],"location":"","date":{"from":"2012-04-23T18:25:43.511Z","to":"2015-04-23T18:25:43.511Z"},"lang":["en","fr","ar","es"],"hasImages":true,"sortbyViews":false,"data_source":["twitter","tumblr"],"urls":["t.co","fb.com"]}}');
+  var send_data = send_data_pre.substring(1,(send_data_pre.length)-1);
+   xhr.send(send_data);
+
+  }
+
   $scope.showAdvanced = function(){
      if (document.getElementById('formAdvanced').style.display == 'block') {
        document.getElementById('formAdvanced').style.display = 'none';
